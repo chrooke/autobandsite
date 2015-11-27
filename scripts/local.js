d03201fec74002696521e48b816a811ed4b6ca9d
Chris Cooke <chrooke@geekdo.org>
2015-11-27 18:56:55 -0500

    Prettier buttons.

--- scripts/local.js
+++ scripts/local.js
@@ -1,11 +1,11 @@
 function togglePlayPause(song) {
     var image = document.getElementById('ppImage.'+song);
     if (image.src.match("play")) {
-        image.src = "./images/Pause.jpg";
+        image.src = "./images/Pause.png";
 	image.alt = "Pause";
 	document.getElementById('player.'+song).play()
     } else {
-        image.src = "./images/play.jpg";
+        image.src = "./images/play.png";
 	image.alt = "Play";
         document.getElementById('player.'+song).pause()
     }
