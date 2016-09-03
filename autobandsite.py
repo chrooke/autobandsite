aceead2a9a1441925755616e7bf740c7d78228b2
Chris Cooke <chrooke@geekido.org>
2016-09-03 11:02:49 -0400

    Making limited ability to 'skin', i.e. keep different look and feel builds that can be chosen to build.

--- autobandsite.py
+++ autobandsite.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import sys
 import os
 import shutil
 from mutagen import File
@@ -17,11 +17,15 @@ siteowner="Chris Cooke"
 siteyear="2016"
 
 #directories
-css="css"
-templates="templates/"
+skin="hierarchy-individual_play_pause"
+skindir="skins/"+skin+"/"
+css=skindir+"css"
+templates=skindir+"templates/"
+images=skindir+"images/"
+scripts=skindir+"scripts/"
+
 songfiles="songfiles/"
-images="images/"
-scripts="scripts/"
+
 build_dir="../autobandsite-build/"
 build_css=build_dir+"css/"
 build_media=build_dir+"media/"
@@ -153,35 +157,3 @@ for album in albums:
         shutil.copyfile(images+"default.jpg",build_images+album.artwork_filename)
 
 # Now we have the raw material to actually generate web pages
-
-#make index page - this shows the most recent album with the option to see more
-with open(build_dir+'index.html','w') as templ:
-    content=album_block(album_list(True)[0])
-    content+='<a href=albums.html>See more albums.</a>'
-    templ.write(fill_in_page(bandname,content).encode('utf8'))
-
-#Build music page - this lists all albums
-with open(build_dir+'albums.html','w') as templ:
-    content=''
-    for album in album_list(True):
-        content+=album_block(album)
-    templ.write(fill_in_page('Albums by '+bandname,content).encode('utf8'))
-
-# build each album page
-for album in album_list():
-    page_title=album.albumname+' by '+ bandname
-    with open(build_albums+album.safe_name+".html",'w') as templ:
-        content=album_block(album)
-        content+='\n<section>\n<div class=songlist">\n<ol>\n'
-        for song in album.tracks():
-            content+=song_list_item_block(song)  
-        content+='\n</ol>\n</div>\n</section>\n'  
-        templ.write(fill_in_page(page_title,content).encode('utf8'))
-        
-#Build each song page - provided as an example
-#for song in songs:
-#    page_title=song.title+' by '+ song.artist
-#    with open(build_songs+song.safe_name+".html",'w') as templ:
-#        content=song_block(song)
-#        templ.write(fill_in_page(page_title,content).encode('utf8'))
-    
