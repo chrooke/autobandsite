f701ea529cba4362cefb3c0ab773cf0062e26f4f
Chris Cooke <chrooke@geekido.org>
2016-09-03 22:00:52 -0400

    Better error handling when building the site. Report the error and automatically cleanup.

--- autobandsite.py
+++ autobandsite.py
@@ -14,18 +14,20 @@ skin="playlist-based"
 skindir="skins/"+skin+"/"
 base="./"
 
+try:
+    # copy the files and directories to current
+    for dir in directories:
+        shutil.copytree(skindir+dir,base+dir)
 
-# copy the files and directories to current
-for dir in directories:
-    shutil.copytree(skindir+dir,base+dir)
-
-for file in files:
-    shutil.copy(skindir+file,base)
+    for file in files:
+        shutil.copy(skindir+file,base)
 
 
-# build the site
-import buildsite
-
+    # build the site
+    import buildsite
+except Exception as e:
+    print(e);
+    print "Uh-oh! There was an error. Site not built. Cleaning up."
 # remove copied files and directories
 import cleanup
 
